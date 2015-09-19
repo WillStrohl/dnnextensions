a8fd7b1cdf6071e854d27120805a52450385f6e3
Will Strohl <will.strohl@gmail.com>
2015-09-19 14:13:56 -0700

    Fixed the copyright statements

--- Modules/CodeCamp/Components/FeatureController.cs
+++ Modules/CodeCamp/Components/FeatureController.cs
@@ -12,7 +12,7 @@
  * list of conditions and the following disclaimer in the documentation and/or 
  * other materials provided with the distribution.
  * 
- * Neither the name of Will Strohl, Disqus Module, nor the names of its contributors may be used 
+ * Neither the name of Will Strohl, nor the names of its contributors may be used 
  * to endorse or promote products derived from this software without specific prior 
  * written permission.
  * 
