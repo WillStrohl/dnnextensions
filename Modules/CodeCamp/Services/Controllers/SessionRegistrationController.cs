a221133aa394acc37d5440104fba28fcd9a26b02
Will Strohl <will.strohl@gmail.com>
2015-09-20 18:20:34 -0700

    Added remaining API end points

---
+++ Modules/CodeCamp/Services/Controllers/SessionRegistrationController.cs
@@ -0,0 +1,177 @@
+﻿/*
+ * Copyright (c) 2015, Will Strohl
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list 
+ * of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this 
+ * list of conditions and the following disclaimer in the documentation and/or 
+ * other materials provided with the distribution.
+ * 
+ * Neither the name of Will Strohl, nor the names of its contributors may be used 
+ * to endorse or promote products derived from this software without specific prior 
+ * written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Web.Api;
+using WillStrohl.Modules.CodeCamp.Entities;
+
+namespace WillStrohl.Modules.CodeCamp.Services
+{
+    public partial class EventController
+    {
+        /// <summary>
+        /// Get all session registrations
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// GET: http://dnndev.me/DesktopModules/CodeCamp/API/Event/GetSessionRegistration
+        /// </remarks>
+        [AllowAnonymous]
+        [HttpGet]
+        public HttpResponseMessage GetSessionRegistrations(int sessionId)
+        {
+            try
+            {
+                var registrations = SessionRegistrationDataAccess.GetItems(sessionId);
+                var response = new ServiceResponse<List<SessionRegistrationInfo>> { Content = registrations.ToList() };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Get a session registration
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// GET: http://dnndev.me/DesktopModules/CodeCamp/API/Event/GetSessionRegistration
+        /// </remarks>
+        [AllowAnonymous]
+        [HttpGet]
+        public HttpResponseMessage GetSessionRegistration(int itemId, int sessionId)
+        {
+            try
+            {
+                var registration = SessionRegistrationDataAccess.GetItem(itemId, sessionId);
+                var response = new ServiceResponse<SessionRegistrationInfo> { Content = registration };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Delete a session registration
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// DELETE: http://dnndev.me/DesktopModules/CodeCamp/API/Event/DeleteSessionRegistration
+        /// </remarks>
+        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        [ValidateAntiForgeryToken]
+        [HttpDelete]
+        public HttpResponseMessage DeleteSessionRegistration(int itemId, int sessionId)
+        {
+            try
+            {
+                SessionRegistrationDataAccess.DeleteItem(itemId, sessionId);
+
+                var response = new ServiceResponse<string> { Content = SUCCESS_MESSAGE };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Create a session registration
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// POST: http://dnndev.me/DesktopModules/CodeCamp/API/Event/CreateSessionRegistration
+        /// </remarks>
+        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public HttpResponseMessage CreateSessionRegistration(SessionRegistrationInfo registration)
+        {
+            try
+            {
+                SessionRegistrationDataAccess.CreateItem(registration);
+
+                var response = new ServiceResponse<string> { Content = SUCCESS_MESSAGE };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Update a session registration
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// POST: http://dnndev.me/DesktopModules/CodeCamp/API/Event/UpdateSessionRegistration
+        /// </remarks>
+        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public HttpResponseMessage UpdateSessionRegistration(SessionRegistrationInfo registration)
+        {
+            try
+            {
+                SessionRegistrationDataAccess.UpdateItem(registration);
+
+                var response = new ServiceResponse<string> { Content = SUCCESS_MESSAGE };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+    }
+}
---
+++ Modules/CodeCamp/Services/Controllers/SpeakerController.cs
@@ -0,0 +1,177 @@
+﻿/*
+ * Copyright (c) 2015, Will Strohl
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list 
+ * of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this 
+ * list of conditions and the following disclaimer in the documentation and/or 
+ * other materials provided with the distribution.
+ * 
+ * Neither the name of Will Strohl, nor the names of its contributors may be used 
+ * to endorse or promote products derived from this software without specific prior 
+ * written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Web.Api;
+using WillStrohl.Modules.CodeCamp.Entities;
+
+namespace WillStrohl.Modules.CodeCamp.Services
+{
+    public partial class EventController
+    {
+        /// <summary>
+        /// Get all speakers
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// GET: http://dnndev.me/DesktopModules/CodeCamp/API/Event/GetSpeakers
+        /// </remarks>
+        [AllowAnonymous]
+        [HttpGet]
+        public HttpResponseMessage GetSpeakers(int codeCampId)
+        {
+            try
+            {
+                var speakers = SpeakerDataAccess.GetItems(codeCampId);
+                var response = new ServiceResponse<List<SpeakerInfo>> { Content = speakers.ToList() };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Get a speaker
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// GET: http://dnndev.me/DesktopModules/CodeCamp/API/Event/GetSpeaker
+        /// </remarks>
+        [AllowAnonymous]
+        [HttpGet]
+        public HttpResponseMessage GetSpeaker(int itemId, int codeCampId)
+        {
+            try
+            {
+                var speaker = SpeakerDataAccess.GetItem(itemId, codeCampId);
+                var response = new ServiceResponse<RoomInfo> { Content = speaker };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Delete a speaker
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// DELETE: http://dnndev.me/DesktopModules/CodeCamp/API/Event/DeleteSpeaker
+        /// </remarks>
+        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        [ValidateAntiForgeryToken]
+        [HttpDelete]
+        public HttpResponseMessage DeleteSpeaker(int itemId, int codeCampId)
+        {
+            try
+            {
+                SpeakerDataAccess.DeleteItem(itemId, codeCampId);
+
+                var response = new ServiceResponse<string> { Content = SUCCESS_MESSAGE };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Create a speaker
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// POST: http://dnndev.me/DesktopModules/CodeCamp/API/Event/CreateSpeaker
+        /// </remarks>
+        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public HttpResponseMessage CreateSpeaker(SpeakerInfo speaker)
+        {
+            try
+            {
+                SpeakerDataAccess.CreateItem(speaker);
+
+                var response = new ServiceResponse<string> { Content = SUCCESS_MESSAGE };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+
+        /// <summary>
+        /// Update a speaker
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// POST: http://dnndev.me/DesktopModules/CodeCamp/API/Event/UpdateSpeaker
+        /// </remarks>
+        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public HttpResponseMessage UpdateSpeaker(SpeakerInfo speaker)
+        {
+            try
+            {
+                SpeakerDataAccess.UpdateItem(speaker);
+
+                var response = new ServiceResponse<string> { Content = SUCCESS_MESSAGE };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response.ObjectToJson());
+            }
+            catch (Exception ex)
+            {
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ERROR_MESSAGE);
+            }
+        }
+    }
+}
