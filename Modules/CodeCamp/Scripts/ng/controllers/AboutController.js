08920615348b602f83de8b143e31710d849fe72c
Will Strohl <will.strohl@gmail.com>
2015-09-27 13:10:17 -0700

    Conditionally showing/hiding code camp info

--- Modules/CodeCamp/Scripts/ng/controllers/AboutController.js
+++ Modules/CodeCamp/Scripts/ng/controllers/AboutController.js
@@ -2,6 +2,28 @@
 
 codeCampApp.controller("aboutController", ["$scope", "$routeParams", "$http", "codeCampServiceFactory", function ($scope, $routeParams, $http, codeCampServiceFactory) {
     
-    
+    var $self = this;
+
+    var factory = codeCampServiceFactory;
+    factory.init(moduleId, moduleName);
+
+    factory.callGetService("GetEventByModuleId")
+        .then(function (response) {
+            var fullResult = angular.fromJson(response);
+            var serviceResponse = JSON.parse(fullResult.data);
+
+            $scope.CodeCamp = serviceResponse.Content;
+            if ($scope.CodeCamp === null) {
+                $scope.HasCodeCamp = false;
+            } else {
+                $scope.HasCodeCamp = true;
+            }
+
+            LogErrors(serviceResponse.Errors);
+        },
+        function (data) {
+            console.log("Unknown error occurred calling GetEventByModuleId");
+            console.log(data);
+        });
 
 }]);
