f278a86def7c2ddf87dc8932f398ade5a67d51a2
Will Strohl <will.strohl@gmail.com>
2015-10-13 19:29:50 -0700

    fixed an intermittent error calling the API

--- Modules/CodeCamp/Scripts/ng/controllers/AboutController.js
+++ Modules/CodeCamp/Scripts/ng/controllers/AboutController.js
@@ -14,63 +14,63 @@ codeCampControllers.controller("aboutController", ["$scope", "$routeParams", "$l
 
             $scope.currentUserId = serviceResponse.Content;
 
+            factory.callGetService("GetEventByModuleId")
+                .then(function (response) {
+                    var fullResult = angular.fromJson(response);
+                    var serviceResponse = JSON.parse(fullResult.data);
+
+                    $scope.codeCamp = serviceResponse.Content;
+
+                    if ($scope.codeCamp === null) {
+                        $scope.hasCodeCamp = false;
+                    } else {
+                        $scope.hasCodeCamp = true;
+
+                        if ($scope.codeCamp != null) {
+                            $scope.codeCamp.BeginDate = ParseDate($scope.codeCamp.BeginDate);
+                            $scope.codeCamp.EndDate = ParseDate($scope.codeCamp.EndDate);
+                        }
+
+                        factory.callGetService("UserCanEditEvent?itemId=" + $scope.codeCamp.CodeCampId)
+                            .then(function (response) {
+                                var fullResult = angular.fromJson(response);
+                                var serviceResponse = JSON.parse(fullResult.data);
+
+                                $scope.userCanEdit = (serviceResponse.Content == "Success");
+
+                                LogErrors(serviceResponse.Errors);
+                            },
+                            function (data) {
+                                console.log("Unknown error occurred calling UserCanEditEvent");
+                                console.log(data);
+                            });
+
+                        factory.callGetService("GetRegistrationByUserId?userId=" + $scope.currentUserId + "&codeCampId=" + $scope.codeCamp.CodeCampId)
+                            .then(function (response) {
+                                var fullResult = angular.fromJson(response);
+                                var serviceResponse = JSON.parse(fullResult.data);
+
+                                $scope.currentUserRegistration = serviceResponse.Content;
+
+                                LogErrors(serviceResponse.Errors);
+                            },
+                            function (data) {
+                                console.log("Unknown error occurred calling GetRegistrationByUserId");
+                                console.log(data);
+                            });
+                    }
+
+                    LogErrors(serviceResponse.Errors);
+                },
+                function (data) {
+                    console.log("Unknown error occurred calling GetEventByModuleId");
+                    console.log(data);
+                });
+
             LogErrors(serviceResponse.Errors);
         },
         function (data) {
             console.log("Unknown error occurred calling GetCurrentUserId");
-            console.log(data);
-        });
-
-    factory.callGetService("GetEventByModuleId")
-        .then(function (response) {
-            var fullResult = angular.fromJson(response);
-            var serviceResponse = JSON.parse(fullResult.data);
-
-            $scope.codeCamp = serviceResponse.Content;
-
-            if ($scope.codeCamp === null) {
-                $scope.hasCodeCamp = false;
-            } else {
-                $scope.hasCodeCamp = true;
-
-                if ($scope.codeCamp != null) {
-                    $scope.codeCamp.BeginDate = ParseDate($scope.codeCamp.BeginDate);
-                    $scope.codeCamp.EndDate = ParseDate($scope.codeCamp.EndDate);
-                }
-
-                factory.callGetService("UserCanEditEvent?itemId=" + $scope.codeCamp.CodeCampId)
-                    .then(function (response) {
-                        var fullResult = angular.fromJson(response);
-                        var serviceResponse = JSON.parse(fullResult.data);
-
-                        $scope.userCanEdit = (serviceResponse.Content == "Success");
-
-                        LogErrors(serviceResponse.Errors);
-                    },
-                    function (data) {
-                        console.log("Unknown error occurred calling UserCanEditEvent");
-                        console.log(data);
-                    });
-
-                factory.callGetService("GetRegistrationByUserId?userId=" + $scope.currentUserId + "&codeCampId=" + $scope.codeCamp.CodeCampId)
-                    .then(function (response) {
-                        var fullResult = angular.fromJson(response);
-                        var serviceResponse = JSON.parse(fullResult.data);
-
-                        $scope.currentUserRegistration = serviceResponse.Content;
-
-                        LogErrors(serviceResponse.Errors);
-                    },
-                    function (data) {
-                        console.log("Unknown error occurred calling GetRegistrationByUserId");
-                        console.log(data);
-                    });
-            }
-
-            LogErrors(serviceResponse.Errors);
-        },
-        function (data) {
-            console.log("Unknown error occurred calling GetEventByModuleId");
             console.log(data);
         });
 
