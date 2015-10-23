e555be105c75e4f25d30a21fa6de0db663bfcc34
Will Strohl <will.strohl@gmail.com>
2015-10-22 20:41:35 -0700

    protecting the permissions of the rooms page

--- Modules/CodeCamp/Scripts/ng/controllers/RoomsController.js
+++ Modules/CodeCamp/Scripts/ng/controllers/RoomsController.js
@@ -2,6 +2,90 @@
 
 codeCampControllers.controller("roomsController", ["$scope", "$routeParams", "$http", "$location", "codeCampServiceFactory", function ($scope, $routeParams, $http, $location, codeCampServiceFactory) {
     
-    
+    var factory = codeCampServiceFactory;
+    factory.init(moduleId, moduleName);
+
+    $scope.LoadData = function () {
+        factory.callGetService("GetCurrentUserId")
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.currentUserId = serviceResponse.Content;
+
+                $scope.LoadEventDetails();
+
+                LogErrors(serviceResponse.Errors);
+            },
+                function (data) {
+                    console.log("Unknown error occurred calling GetCurrentUserId");
+                    console.log(data);
+                });
+    }
+
+    $scope.LoadEventDetails = function () {
+        factory.callGetService("GetEventByModuleId")
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.codeCamp = serviceResponse.Content;
+
+                if ($scope.codeCamp === null) {
+                    $scope.hasCodeCamp = false;
+                } else {
+                    $scope.hasCodeCamp = true;
+
+                    if ($scope.codeCamp != null) {
+                        $scope.codeCamp.BeginDate = ParseDate($scope.codeCamp.BeginDate);
+                        $scope.codeCamp.EndDate = ParseDate($scope.codeCamp.EndDate);
+                    }
+
+                    $scope.LoadEditPermissions();
+
+                    $scope.getCurrentUserId();
+                }
+
+                LogErrors(serviceResponse.Errors);
+            },
+                function (data) {
+                    console.log("Unknown error occurred calling GetEventByModuleId");
+                    console.log(data);
+                });
+    }
+
+    $scope.LoadEditPermissions = function () {
+        factory.callGetService("UserCanEditEvent?itemId=" + $scope.codeCamp.CodeCampId)
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.userCanEdit = (serviceResponse.Content == "Success");
+
+                LogErrors(serviceResponse.Errors);
+            },
+                function (data) {
+                    console.log("Unknown error occurred calling UserCanEditEvent");
+                    console.log(data);
+                });
+    }
+
+    $scope.getCurrentUserId = function () {
+        factory.callGetService("GetCurrentUserId")
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.currentUserId = serviceResponse.Content;
+
+                LogErrors(serviceResponse.Errors);
+            },
+                function (data) {
+                    console.log("Unknown error occurred calling GetCurrentUserId");
+                    console.log(data);
+                });
+    }
+
+    $scope.LoadData();
 
 }]);
