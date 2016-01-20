6f0e630cbbc67c18b3ea404c838321b72121e387
Will Strohl <will.strohl@gmail.com>
2016-01-19 18:02:30 -0800

    added placeholder DAL and template content

--- Modules/CodeCamp/Scripts/ng/controllers/AgendaController.js
+++ Modules/CodeCamp/Scripts/ng/controllers/AgendaController.js
@@ -2,8 +2,115 @@
 
 codeCampControllers.controller("agendaController", ["$scope", "$routeParams", "$http", "$location", "codeCampServiceFactory", function ($scope, $routeParams, $http, $location, codeCampServiceFactory) {
     
+    var factory = codeCampServiceFactory;
+    factory.init(moduleId, moduleName);
+
+    $scope.LoadData = function () {
+        factory.callGetService("GetEventByModuleId")
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.codeCamp = serviceResponse.Content;
+
+                if ($scope.codeCamp != null) {
+                    $scope.codeCamp.BeginDate = ParseDate($scope.codeCamp.BeginDate);
+                    $scope.codeCamp.EndDate = ParseDate($scope.codeCamp.EndDate);
+                }
+
+                if ($scope.codeCamp === null) {
+                    $scope.hasCodeCamp = false;
+                } else {
+                    $scope.hasCodeCamp = true;
+
+                    $scope.LoadEditPermissions();
+                }
+
+                LogErrors(serviceResponse.Errors);
+            },
+                function (data) {
+                    console.log("Unknown error occurred calling GetEventByModuleId");
+                    console.log(data);
+                });
+    }
+
+    $scope.LoadEditPermissions = function () {
+        factory.callGetService("UserCanEditEvent?itemId=" + $scope.codeCamp.CodeCampId)
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.userCanEdit = (serviceResponse.Content == "Success");
+
+                $scope.LoadTracks();
+
+                LogErrors(serviceResponse.Errors);
+            },
+            function (data) {
+                console.log("Unknown error occurred calling UserCanEditEvent");
+                console.log(data);
+            });
+    }
+
+    $scope.LoadTracks = function () {
+        factory.callGetService("GetTracks?codeCampId=" + $scope.codeCamp.CodeCampId)
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.tracks = serviceResponse.Content;
+
+                $.each($scope.tracks, function (i, track) {
+                    track.TrackSlug = GetSlugFromValue(track.Title);
+                });
+
+                $scope.LoadTimeSlots();
+
+                LogErrors(serviceResponse.Errors);
+            },
+            function (data) {
+                console.log("Unknown error occurred calling GetTracks");
+                console.log(data);
+            });
+    };
+
+    $scope.LoadTimeSlots = function () {
+        factory.callGetService("GetTimeSlots?codeCampId=" + $scope.codeCamp.CodeCampId)
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.timeSlots = serviceResponse.Content;
+
+                LogErrors(serviceResponse.Errors);
+            },
+                function (data) {
+                    console.log("Unknown error occurred calling GetTimeSlots");
+                    console.log(data);
+                });
+    }
+
+    $scope.LoadSessions = function (trackId) {
+        factory.callGetService("GetSessionsByTrackId?trackId=" + trackId + "&codeCampId=" + $scope.codeCamp.CodeCampId)
+            .then(function (response) {
+                var fullResult = angular.fromJson(response);
+                var serviceResponse = JSON.parse(fullResult.data);
+
+                $scope.assignedSessions = serviceResponse.Content;
+
+                LogErrors(serviceResponse.Errors);
+            },
+                function (data) {
+                    console.log("Unknown error occurred calling GetSessionsByTrackId");
+                    console.log(data);
+                    return null;
+                });
+    };
+
     $scope.goToPage = function (pageName) {
         $location.path(pageName);
     }
+
+    $scope.LoadData();
 
 }]);
