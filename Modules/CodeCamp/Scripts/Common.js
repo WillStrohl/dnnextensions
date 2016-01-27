11e6ff437b4b4311e06f17c78a786d9836dbd158
Will Strohl <will.strohl@gmail.com>
2016-01-27 00:35:51 -0800

    added a JS method to allow objects to be copied (cloned) so they're not copied by reference

--- Modules/CodeCamp/Scripts/Common.js
+++ Modules/CodeCamp/Scripts/Common.js
@@ -44,3 +44,16 @@ function GetSlugFromValue(value) {
 function addMinutes(date, minutes) {
     return new Date(date.getTime() + minutes * 60000);
 }
+
+function cloneObject(obj) {
+    if (obj === null || typeof obj !== "object") {
+        return obj;
+    }
+
+    var temp = obj.constructor(); // give temp the original obj's constructor
+    for (var key in obj) {
+        temp[key] = cloneObject(obj[key]);
+    }
+
+    return temp;
+}
