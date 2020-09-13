b5aa847c6e846b91d3806ed9a45bc99229f7ea48
Will Strohl <will@upendoventures.com>
2020-09-13 10:48:34 -0700

    Added a sponsor view

---
+++ Modules/WillStrohl.LightboxGallery/AboutMe.ascx
@@ -0,0 +1,20 @@
+﻿<%@ Control Language="VB" AutoEventWireup="true" CodeBehind="AboutMe.ascx.vb" Inherits="WillStrohl.Modules.Lightbox.AboutMe" %>
+
+<div class="dnnClear" style="margin: 0 auto; width: 800px; max-width: 800px;">
+    <h1><%=GetLocalizedString("About") %></h1>
+    <p><%=GetLocalizedString("ThankYou") %></p>
+    <div class="dnnClear" style="width: 50%; margin: 0 auto;">
+        <div class="dnnLeft">
+            <p class="DNNAligncenter">
+                <a href="https://github.com/hismightiness/dnnextensions/wiki" class="dnnSecondaryAction" target="_blank"><%=GetLocalizedString("Help") %></a>
+            </p>
+        </div>
+        <div class="dnnRight">
+            <p class="DNNAligncenter">
+                <a href="https://github.com/sponsors/hismightiness" target="_blank" class="dnnPrimaryAction">
+                    <%=GetLocalizedString("SponsorMe") %>
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
---
+++ Modules/WillStrohl.LightboxGallery/AboutMe.ascx.vb
@@ -0,0 +1,31 @@
+﻿'
+' Copyright Upendo Ventures, LLC
+' https://upendoventures.com
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+' and associated documentation files (the "Software"), to deal in the Software without restriction,
+' including without limitation the rights to use, copy, modify, merge, publish, distribute,
+' sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all copies or
+' substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+' BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+' NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+' DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF Or IN CONNECTION WITH THE SOFTWARE Or THE USE Or OTHER DEALINGS IN THE SOFTWARE.
+'
+
+Namespace WillStrohl.Modules.Lightbox
+
+    Partial Public MustInherit Class AboutMe
+        Inherits WnsPortalModuleBase
+
+        Private Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        End Sub
+
+    End Class
+
+End Namespace
